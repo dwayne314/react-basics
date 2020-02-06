6b19dea4cf6d42d00ea61996457b364b14c5f984
dwayne314 <dwayne.manlove@gmail.com>
2020-02-06 00:28:32 -0500

    Created the header / Added Initial Routes
    Routes Created:
        * Home
        * Login
        * Settings

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
+import './Header.css';
 
 const Header = () => {
 	return (
 		<div className="header">
-			
+			<div className="title-container">
+				<div className="title">
+					<Link to="/">		
+						Tic-Tac-Toe
+					</Link>
+				</div>
+			</div>
+			<span className="header-links-container">
+				<div className="header-links">
+					<Link to="/login">
+						<span className="header-link">Login</span>
+					</Link>
+					<Link to="/settings">
+						<span className="header-link">Settings</span>
+					</Link>
+				</div>
 
+			</span>
 		</div>
 	);
 };
